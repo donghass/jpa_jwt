ca7c4201d3574b5718832847359c730a33ec75ce
donghass <ehdgk1221@gmail.com>
2025-01-15 21:30:21 +0900

    Spring JPA와 jwt 토큰을 이용한 회원가입

---
+++ src/main/java/org/fastcampus/community_feed/auth/application/interfaces/UserAuthRepository.java
@@ -0,0 +1,9 @@
+package org.fastcampus.community_feed.auth.application.interfaces;
+
+import org.fastcampus.community_feed.auth.domain.UserAuth;
+import org.fastcampus.community_feed.user.domain.User;
+
+public interface UserAuthRepository {
+    void registerUser(UserAuth userAuth, User user);
+    UserAuth findByEmail(String email);
+}
